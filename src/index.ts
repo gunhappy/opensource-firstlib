06c7c839dfdeaacfb3e3a146cb3e569dd10b1f74
admin <phasin@wongnai.com>
2019-09-13 15:29:05 +0700

    add inquire

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
 #!/usr/bin/env node
-
-import program from 'commander'
-import Package from '../package.json'
+import commander from 'commander'
+import inquirer from 'inquirer'
 
 export function add(a: number, b: number) {
   return a + b
 }
 
-program.version(Package.version).parse(process.argv)
+commander.version('1.0.0')
+
+commander
+  .command('love <thing>')
+  .option('-h, --hate <thing>')
+  .action((thing, option) => {
+    console.log(`I love ${thing}`)
+    if (option.hate) {
+      console.log(`I hate ${option.hate}`)
+    }
+  })
+
+commander.command('ask').action(async () => {
+  const answers = await inquirer.prompt([
+    {
+      choices: ['React', 'Vue', 'Angular'],
+      message: 'What is the best framework',
+      name: 'framework',
+      type: 'list',
+    },
+  ])
+  if (answers.framework === 'React') {
+    console.log('Yes You are 10x Engineer')
+  } else {
+    console.log('Fuck you monkey')
+  }
+})
+
+commander.parse(process.argv)
